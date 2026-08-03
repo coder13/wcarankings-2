2565eed55183b70d384218ce5ff04cf21883b29a
Cailyn Sinclair <choover11@gmail.com>
2026-08-02 21:07:35 -0700

    Add title for list browser

--- app/lists/page.tsx
+++ app/lists/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next";
 import { ListBrowse } from "@/components/ListBrowse/ListBrowse";
+
+export const metadata: Metadata = {
+  title: "Lists | WCA Rankings",
+};
 
 export default function ListsPage() {
   return <ListBrowse />;
